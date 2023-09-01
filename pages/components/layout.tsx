dc2ba1bc584bb4f9a9c2ccc04c7466fc9a630fab
kimneighbor <designkimdh@gmail.com>
2023-09-01 17:29:27 +0900

    update index

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -10,7 +10,6 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Header />
-      <div>layout</div>
       {/* children 은 하위 컴포넌트를 의미한다. 즉 보여지는 위치를 지정할 수 있음 */}
       <div>{children}</div>
       <Footer />
