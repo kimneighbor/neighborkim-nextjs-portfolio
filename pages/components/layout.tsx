52b4017329fe1f7ea4498c7acd8f2b1cd5ba9c36
kimneighbor <designkimdh@gmail.com>
2023-09-01 16:50:52 +0900

    add type annotations for layout

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -1,10 +1,19 @@
-export default function Layout({ children }:any) {
+import Header from "./header";
+import Footer from "./footer";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Header />
       <div>layout</div>
       {/* children 은 하위 컴포넌트를 의미한다. 즉 보여지는 위치를 지정할 수 있음 */}
       <div>{children}</div>
-
+      <Footer />
     </>
   );
 }
