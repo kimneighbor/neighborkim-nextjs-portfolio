bf334c629257e3cd7e9f526df060a634c9e96494
kimneighbor <designkimdh@gmail.com>
2023-09-01 17:46:25 +0900

    add animation

---
+++ pages/components/home/hero.tsx
@@ -0,0 +1,25 @@
+export default function Hero() {
+  return (
+    <>
+    <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+    <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          안녕하세요 열심히 배우는 개발자
+          <br className="hidden lg:inline-block"/>
+          김이웃 입니다.
+        </h1>
+        <p className="mb-8 leading-relaxed">
+        그러므로 꽃이 따뜻한 불어 약동하다. 구하지 우리의 희망의 찾아다녀도, 싸인 있다. 속에서 이 위하여 능히 아니다. 따뜻한 놀이 없는 피가 이상의 황금시대의 사람은 이것이다. 위하여, 설레는 눈이 듣는다. 군영과 거친 곳으로 하여도 위하여 황금시대다. 지혜는 착목한는 얼마나 아니한 눈에 피가 청춘의 약동하다. 끝에 열락의 미인을 착목한는 두기 인간은 위하여서. 방지하는 있는 피가 소리다.이것은 실로 가슴에 운다. 많이 귀는 오아이스도 풀이 청춘의 인간의 이 같이, 아니다. 옷을 구하지 위하여 새가 웅대한 것이다.
+        </p>
+        <div className="flex justify-center">
+          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">프로젝트 보러가기</button>
+        </div>
+      </div>
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      </div>
+    </div>
+  </section>
+  </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Layout from './components/layout'
 import Head from 'next/head'
+import Hero from './components/home/hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,27 +15,7 @@ export default function Home() {
         <meta name="description" content="test content~!"></meta>
         <link rel="icon" href="/favicon.ico" />
         </Head>
-
-
-        <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                끊임없이 배우는 개발자
-                <br className="hidden lg:inline-block"/>
-                김이웃 입니다.
-              </h1>
-              <p className="mb-8 leading-relaxed">
-                Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-              <div className="flex justify-center">
-                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">프로젝트 보러가기</button>
-              </div>
-            </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            </div>
-          </div>
-        </section>
-        
+        <Hero/>
         </Layout>
         
 
