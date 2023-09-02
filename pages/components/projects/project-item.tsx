fb02179c0b3a27db811a116f3abd7e3c353a1841
kimneighbor <designkimdh@gmail.com>
2023-09-02 23:10:56 +0900

    update project-item.tsx

--- pages/components/projects/project-item.tsx
+++ pages/components/projects/project-item.tsx
@@ -1,6 +1,7 @@
-export default function ProjectItem(){
+export default function ProjectItem({data}:any){
     return(
-        <div>
+        <div className="p-6 bg-slate-400 rounded-md">
+            <h1>{data.properties.Name.title[0].plain_text}</h1>
         </div>
     )
 }
