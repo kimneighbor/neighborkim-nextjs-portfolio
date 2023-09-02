2d7c8ecf71f48d249ebd7a65d449861e3cdebe33
kimneighbor <designkimdh@gmail.com>
2023-09-02 22:55:50 +0900

    update projects

--- config/index.tsx
+++ config/index.tsx
@@ -1,3 +1,2 @@
 export const DATABASE_ID = process.env.NOTION_DATABASE_ID
-
-console.log(DATABASE_ID)
+export const TOKEN = process.env.NOTION_TOKEN
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,18 +1,28 @@
-import Link from "next/link";
 import Layout from "./components/layout";
+import {TOKEN, DATABASE_ID} from "@/config";
+import HEAD from "next/head";
 
-export default function Projects() {
+
+export default function Projects({projects}: any) {
+    // console.log(projectsNames)
     return (
         <Layout>
-            <title>NeighborKim.com</title>
-            <meta name="description" content="Learn Next.js"></meta>
-            <link rel="icon" href="/favicon.ico" />
-             <h1>프로젝트 파일</h1>
+            <HEAD>
+                <title>NeighborKim.com</title>
+                <meta name="description" content="Learn Next.js"></meta>
+                <link rel="icon" href="/favicon.ico"/>
+            </HEAD>
+
+            <h1>프로젝트 진행 수 : {projects.results.length}</h1>
+
+            {projects.results.map((aProject: any) => (
+                <h1>{aProject.properties.Name.title[0].plain_text}</h1>
+            ))}
+
 
         </Layout>
     );
 }
-
 
 
 // 빌드 타임에 호출 (딱 1번)
@@ -23,18 +33,34 @@ export async function getStaticProps() {
         headers: {
             accept: 'application/json',
             'Notion-Version': '2022-06-28',
-            'content-type': 'application/json'
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+
         },
-        body: JSON.stringify({page_size: 100})
+        body: JSON.stringify({
+            // 정렬 적용
+            sorts: [
+                {
+                    "property": "Name",
+                    "direction": "ascending"
+                }
+            ],
+            page_size: 100
+        })
     };
 
-    fetch('https://api.notion.com/v1/databases/database_id/query', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
+    const projects = await res.json()
+
+
+    const projectNames = projects.results.map((aProject: any) => (
+        aProject.properties.Name.title[0].plain_text
+    ))
+
 
     return {
         props: {
+            projects
             // allPostsData
         }
     };
