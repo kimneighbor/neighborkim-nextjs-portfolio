c077b2c7ece036edf51e4ada86c483dfaf100374
kimneighbor <designkimdh@gmail.com>
2023-09-05 19:04:28 +0900

    fix image

--- components/projects/project-item.tsx
+++ components/projects/project-item.tsx
@@ -1,4 +1,5 @@
 import {GitHubLink, VelogLink} from '../icon-button';
+import Image from "next/legacy/image";
 
 export default function ProjectItem({data}: any) {
 
@@ -18,7 +19,14 @@ export default function ProjectItem({data}: any) {
         <div className="xl:w-1/4 md:w-1/2 p-3">
             <div
                 className="bg-white dark:bg-slate-800 rounded-lg transition-colors duration-1000 ease-in-out h-full">
-                <img className="rounded-t-md object-cover object-center" src={imgSrc} alt="content"/>
+                {/*<img className="rounded-t-md object-cover object-center" src={imgSrc} alt="content"/>*/}
+                <Image className="rounded-t-md object-cover object-center"
+                       src={imgSrc}
+                       alt="content"
+                       width={768}
+                       height={432}
+                />
+
                 <div className="p-5">
                     <h3 className="tracking-widest dark:text-blue-400 text-blue-600 text-xs font-medium title-font">{subtitle}</h3>
                     <h2 className="text-xl text-gray-900 font-medium title-font">{title}</h2>
@@ -27,24 +35,25 @@ export default function ProjectItem({data}: any) {
                         {description.length > 41 ? `${description.substring(0, 40)}...` : description}
                     </p>
 
+                    {/*Tags*/}
                     <div className="flex items-start mb-3 ">
                         {tags.map((aTag: any) => (
-                            <h2 className="text-sm px-2 py-0.5 mr-2 rounded-md bg-blue-200 dark:bg-blue-600 "
+                            <h2 className="text-xs px-2 py-0.5 mr-2 rounded-md bg-blue-200 dark:bg-blue-600 "
                                 key={aTag.id}>{aTag.name}</h2>
                         ))}
                     </div>
 
-                        {/*조건부 렌더링. github가 있으면 아이콘을 보여주고 없으면 아이콘을 보여주지 않는다.*/}
-                        {github && (
-                            <a href={github} target="_blank" rel="noopener noreferrer">
-                                <GitHubLink/>
-                            </a>
-                        )}
-                        {velog && (
-                            <a href={velog} target="_blank" rel="noopener noreferrer">
-                                <VelogLink/>
-                            </a>
-                        )}
+                    {/*조건부 렌더링. github가 있으면 아이콘을 보여주고 없으면 아이콘을 보여주지 않는다.*/}
+                    {github && (
+                        <a href={github} target="_blank" rel="noopener noreferrer">
+                            <GitHubLink/>
+                        </a>
+                    )}
+                    {velog && (
+                        <a href={velog} target="_blank" rel="noopener noreferrer">
+                            <VelogLink/>
+                        </a>
+                    )}
 
                 </div>
             </div>
