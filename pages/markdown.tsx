0c6eed3f85492806288a9d6bd05d189e4cbebbd9
kimneighbor <designkimdh@gmail.com>
2023-09-08 19:39:31 +0900

    add markdown test

--- pages/markdown.tsx
+++ pages/markdown.tsx
@@ -3,27 +3,121 @@ import Layout from "@/components/layout";
 import ReactMarkdown from "react-markdown";
 
 
-
-
 const markdowntext = `
-### lottie 재생
 
-- \`npm install --save react-lottie-player\`
+# -
 
-# 9 버전 이후 <Link> 태그 안에 <a> 태그 사용 불가
 
-https://nextjs.org/docs/messages/invalid-new-link-with-extra-anchor
+# 👋 React 프론트엔드 개발, Go 백엔드 개발 등에 숙련된 **10년 차 소프트웨어 엔지니어**입니다. 현재 **서울**에 있습니다.
 
-9버전 이전에 작성된 코드에서 사용하려면 \`legacyBehavior\` 를 사용하는 방법이 있다. 
 
-\`\`\`jsx
-// 예시
-<Link href="/" legacyBehavior>
-\`\`\`
 
-# min-h-screen 헤더에 밀리는 현상
+### 연락처
 
-본문에 min-h-screen 을 적용 시키면 세로로 정렬이 되지만 화면상의 가운데가 아닌 header 를 제외한 영역의 가운데로 정렬이 된다. min-h-[calc(100vh-헤더height크기)] 로 임시 해결
+[📧 adalovelace@mail.com](mailto:adalovelace@mail.com)
+
+[🐦 twitter.com/adalovelace](http://twitter.com/notionhq)
+
+[🔗 linkedin.com/in/adalovelace](https://www.linkedin.com/company/notionhq/mycompany/)
+
+[👾 github.com/adalovelace](https://github.com/makenotion)
+
+# 경력
+
+### 소프트웨어 엔지니어 **II**
+
+**K사***, 서울시 강남구 – (2019년 12월 - 현재)*
+
+- 통합과 개발자 툴을 담당하는 팀의 풀스택 엔지니어로 K 플랫폼의 개발자 경험을 정의하고 개선하는 일을 담당합니다.
+- 초기에는 프론트엔드에 중점을 두고 React, Redux, Styled Components, Jest 등을 사용해 OAuth 구현, 개인 토큰 및 Webhook을 위한 프론트엔드 재구축을 주도했습니다.
+- PostgreSQL과 Node.js(Express/Hapi) 및 GoLang으로 구축된 백엔드 웹훅 및 통합 개발에 참여했습니다.
+- 팀에 합류한 초급 개발자 코치로서 페어 프로그래밍과 몹 프로그래밍 세션을 진행하고, 시니어 엔지니어와 함께 로드맵 및 기능 우선순위를 정의했습니다.
+
+### QA 자동화 엔지니어
+
+**J사***, 서울시 강남구  – (2014년 5월 - 2019년 10월)*
+
+- Node.js, Mocha, Chai와 Behat 및 PHP를 사용해 회사의 핵심 API 및 결제 API를 위한 자동 인수/계약 테스트를 구축했습니다. API 설계 및 기본 로직의 일관성을 유지하기 위해 새로운 기능 개발을 검토하고 검증했습니다.
+- 기술 분야에서의 다양성과 여성(Diversity and Women in Tech) 이니셔티브에 적극적으로 참여하면서 정기적인 전사 미팅과 다양한 제안을 추진했습니다. 직장 동료의 강연 제안서 작성을 도왔습니다.
+
+### 소프트웨어 테스터
+
+**C사***, 대전광역시 유성구 – (2012년 8월 - 2014년 4월)*
+
+- 선적 컨테이너 터미널 및 물류 관리 웹 앱의 기능 테스트를 계획하고 실행했습니다.
+- 새로운 클라이언트 요구 사항을 평가하고, 사용 사례(use case)를 정의했으며, 로컬 테스트 환경을 유지보수했습니다.
+
+### **QA 분석가**
+
+**B사***, 서울시 강남구 – (2011년 4월 - 2012년 6월)*
+
+- 광고대행사 B에서 다양하고 복잡한 프로젝트의 QA를 혼자 수행했습니다. 여러 완성차 고객사의 웹사이트 내 툴과 애플리케이션의 카피, 사용자 흐름 및 기능을 검증하고 탐색적 테스트를 수행했습니다.
+- PM 및 개발자 팀과 원격으로 커뮤니케이션하고, 견적 및 우선순위 지정을 담당했습니다.
+
+---
+
+# 기술
+
+### Javascript
+
+저의 주력 언어 입니다. 6년 이상 React와 Redux를 사용해 프론트엔드 개발, Node, Express, Hapi.js로 백엔드 API 개발, Mocha와 Jest로 자동화 테스트도 진행했습니다.
+
+### Golang
+
+최근에 배우기 시작했고 어렵지만 재미있었습니다. 아직 능숙하지는 않지만 직접 몇몇 기능을 만들어보고 픽스를 적용했습니다.
+
+### 기술 문서 작성
+
+기술적인 주제에 대해 글 쓰는 것을 좋아하고 잘 합니다. 간단한 용어로 설명해 사람들이 쉽게 이해하도록 하는 것에 보람을 느낍니다.
+
+---
+
+# 언어
+
+### 한국어 🇰🇷
+
+원어민
+
+### 영어 🇺🇸
+
+상급 - TOEIC 940
+
+---
+
+# 기사와 강연
+
+- [OAuth와 춤을: 단계별 가이드](https://dev.to/anabella/dancing-with-oauth-emp)
+- [PassportJS의 OAuth 흐름 들여다보기](https://dev.to/anabella/a-peep-beneath-the-hood-of-passportjs-oauth-flow-eb5)
+- [왜 훌륭한 개발자는 필기 시험을 좋아할까?](https://dev.to/anabella/why-do-great-developers-love-writing-tests-1o6j)
+- [초급, 중급, 시니어 개발자가 바에 들어가는데...](https://dev.to/anabella/a-junior-a-mid-and-a-senior-dev-walk-into-a-bar-414f)
+- [면접을 위한 자신감 도구 상자](https://dev.to/typeform/the-self-confidence-toolbox-for-job-interviews-4k3j)
+- [CSS 포지션 버전의 다양한 페어링 이해하기](https://dev.to/anabella/understanding-the-different-pairings-of-css-position-flavours-5855)
+- [두 가지 초밥 조리법으로 CSS 플로팅 파악하기](https://dev.to/anabella/figuring-out-css-floats-with-sushi-recipes-2c5o)
+
+🗣 **강연:** [OAuth Dance 배우기](https://www.youtube.com/watch?v=ec2NBIoaUCM&t=1s) (APIdays Paris Dec 2018)
+
+---
+
+# 학력
+
+## **컴퓨터 공학 학사**
+
+2017년 졸업
+
+ㅇㅇ대학교 - 서울특별시
+
+---
+
+# 연락처
+
+[📧 adalovelace@mail.com](mailto:adalovelace@mail.com)
+
+[🐦 twitter.com/adalovelace](http://twitter.com/notionhq)
+
+[🔗 linkedin.com/in/adalovelace](https://www.linkedin.com/company/notionhq/mycompany/)
+
+[👾 github.com/adalovelace](https://github.com/makenotion)
+
 `
 
 
@@ -37,17 +131,16 @@ export default function Markdown() {
             </HEAD>
 
 
-
-            <section className="flex min-h-[calc(100vh-180px)] flex-col items-center justify-center text-gray-600">
-
-
-                <div className=" mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-
-                    <ReactMarkdown className="markdown items-start">
-                        {markdowntext}
-                    </ReactMarkdown>
-
-
+            <section className="text-gray-500 relative">
+                <div className="container
+                sm:mx-auto px-10 md:px-24 lg:px-48 2xl:px-96 mx-auto
+                py-24
+                 ">
+                    <div className="flex flex-col text-left w-full">
+                        <ReactMarkdown className="markdown leading-8">
+                            {markdowntext}
+                        </ReactMarkdown>
+                    </div>
                 </div>
             </section>
         </Layout>
