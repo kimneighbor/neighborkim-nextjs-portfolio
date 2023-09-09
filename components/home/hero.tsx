f9e1814b82ded3f870177731ecd14b5e58b5399a
kimneighbor <designkimdh@gmail.com>
2023-09-09 13:42:03 +0900

    modify css

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -6,8 +6,8 @@ export default function Hero() {
     <>
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center slide-up slide-up">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 ">
-          디자인 하는 개발자
-          <hr/>김동환 입니다.
+          디자인하는 개발자
+          <hr/>김동환입니다.
         </h1>
 
         <p className="mb-8 leading-relaxed">
